2361159bb7eef29c98523fdbe0f1f97151f56f2b
Jameson <james.bradlee@telenor.no>
2023-08-24 02:15:02 +0200

    v0.1.2

--- src/main/kotlin/no/ghpkg/Main.kt
+++ src/main/kotlin/no/ghpkg/Main.kt
@@ -8,37 +8,39 @@ import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository
 import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.plugins.PublishingPlugin
 import java.net.URI
 
 class GithubPackagesPlugin : Plugin<Project> {
-    override fun apply(project: Project) {
-        installOn(project, project.repositories)
-        if (project.project.plugins.hasPlugin("maven-publish")) {
-            val allProjectsRepositories =
-                (project.allprojects as ExtensionAware).extensions.getByType(PublishingExtension::class.java).repositories
-            val subProjectsRepositories =
-                (project.subprojects as ExtensionAware).extensions.getByType(PublishingExtension::class.java).repositories
-            installOn(project, allProjectsRepositories)
-            installOn(project, subProjectsRepositories)
-        }
-    }
 
-    private fun installOn(project: Project, repositories: RepositoryHandler) {
-        (repositories as ExtensionAware).extensions.create("git", Git::class.java, project, repositories)
-    }
+	override fun apply(project: Project) {
+		installOn(project, project.repositories)
+		project.plugins.withType(PublishingPlugin::class.java) {
+			installOn(project, project.extensions.getByType(PublishingExtension::class.java))
+		}
+	}
+
+	private fun installOn(project: Project, repositories: RepositoryHandler) {
+		(repositories as ExtensionAware).extensions.create("git", Git::class.java, project, repositories)
+	}
+
+	private fun installOn(project: Project, publishing: PublishingExtension) {
+		installOn(project, publishing.repositories)
+	}
 }
 
-private val INVALID_CHARS = Regex("[^A-Za-z0-9_\\-.]+")
-private val MANY_DASH = Regex("-+")
-private fun fixRepoName(name: String) = name.replace(INVALID_CHARS, "-").replace(MANY_DASH, "-")
+private val INVALID_CHARS = Regex("[^A-Za-z0-9_.]+")
+private fun fixRepoName(name: String) = name.replace(INVALID_CHARS, "-")
 
 open class Git(private val project: Project, private val repositories: RepositoryHandler) {
-    fun hub(owner: String, repository: String): MavenArtifactRepository = repositories.maven { repo ->
-        repo.name = fixRepoName("GithubPackages $owner/$repository")
-        repo.url = URI.create("https://maven.pkg.github.com/$owner/$repository")
-        repo.credentials { cred ->
-            cred.username = project.findProperty("gpr.actor") as String? ?: System.getenv("GITHUB_ACTOR")
-            cred.password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
-        }
-    }
+	fun hub(owner: String, repository: String): MavenArtifactRepository = repositories.maven { repo ->
+		repo.name = fixRepoName("GithubPackages $owner/$repository")
+		repo.url = URI.create("https://maven.pkg.github.com/$owner/$repository")
+		repo.credentials { cred ->
+			cred.username = project.findProperty("gpr.actor") as String? ?: System.getenv("GITHUB_ACTOR")
+				?: throw Exception("\$GITHUB_ACTOR (or gpr.actor from ~/.gradle/gradle.properties) was not found!")
+			cred.password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+				?: throw Exception("\$GITHUB_TOKEN (or gpr.token from ~/.gradle/gradle.properties) was not found!")
+		}
+	}
 }
